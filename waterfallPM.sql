88e93a733fb4a77e498f3dfd5875d458b15e1b8f
rrecurse <christopherdebellis@gmail.com>
2016-02-18 21:25:14 -0500

    Added database/mysqldump skeleton (no data)

---
+++ waterfallPM.sql
@@ -0,0 +1,450 @@
+-- MySQL dump 10.13  Distrib 5.1.73, for redhat-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: pm
+-- ------------------------------------------------------
+-- Server version	5.1.73
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `chat`
+--
+
+DROP TABLE IF EXISTS `chat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chat` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `time` varchar(255) NOT NULL DEFAULT '',
+  `ufrom` varchar(255) NOT NULL DEFAULT '',
+  `ufrom_id` int(10) NOT NULL DEFAULT '0',
+  `userto` varchar(255) NOT NULL DEFAULT '',
+  `userto_id` int(10) NOT NULL DEFAULT '0',
+  `text` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `company`
+--
+
+DROP TABLE IF EXISTS `company`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `company` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `company` varchar(255) NOT NULL,
+  `contact` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(64) NOT NULL,
+  `mobile` varchar(64) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `zip` varchar(16) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `desc` text NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `company_assigned`
+--
+
+DROP TABLE IF EXISTS `company_assigned`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `company_assigned` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `user` int(10) NOT NULL,
+  `company` int(10) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `files`
+--
+
+DROP TABLE IF EXISTS `files`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `files` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `desc` varchar(255) NOT NULL DEFAULT '',
+  `project` int(10) NOT NULL DEFAULT '0',
+  `milestone` int(10) NOT NULL DEFAULT '0',
+  `user` int(10) NOT NULL DEFAULT '0',
+  `tags` varchar(255) NOT NULL DEFAULT '',
+  `added` varchar(255) NOT NULL DEFAULT '',
+  `datei` varchar(255) NOT NULL DEFAULT '',
+  `type` varchar(255) NOT NULL DEFAULT '',
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `folder` int(10) NOT NULL,
+  `visible` text NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `name` (`name`),
+  KEY `datei` (`datei`),
+  KEY `added` (`added`),
+  KEY `project` (`project`),
+  KEY `tags` (`tags`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `files_attached`
+--
+
+DROP TABLE IF EXISTS `files_attached`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `files_attached` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `file` int(10) unsigned NOT NULL DEFAULT '0',
+  `message` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `file` (`file`,`message`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `log`
+--
+
+DROP TABLE IF EXISTS `log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `log` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `user` int(10) NOT NULL DEFAULT '0',
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `type` varchar(255) NOT NULL DEFAULT '',
+  `action` int(1) NOT NULL DEFAULT '0',
+  `project` int(10) NOT NULL DEFAULT '0',
+  `datum` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`ID`),
+  KEY `datum` (`datum`),
+  KEY `type` (`type`),
+  KEY `action` (`action`),
+  FULLTEXT KEY `username` (`username`),
+  FULLTEXT KEY `name` (`name`)
+) ENGINE=MyISAM AUTO_INCREMENT=130 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `messages` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `project` int(10) NOT NULL DEFAULT '0',
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `text` text NOT NULL,
+  `tags` varchar(255) NOT NULL,
+  `posted` varchar(255) NOT NULL DEFAULT '',
+  `user` int(10) NOT NULL DEFAULT '0',
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `replyto` int(11) NOT NULL DEFAULT '0',
+  `milestone` int(10) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `project` (`project`),
+  KEY `user` (`user`),
+  KEY `replyto` (`replyto`),
+  KEY `tags` (`tags`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `milestones`
+--
+
+DROP TABLE IF EXISTS `milestones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `milestones` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `project` int(10) NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `desc` text NOT NULL,
+  `start` varchar(255) NOT NULL DEFAULT '',
+  `end` varchar(255) NOT NULL DEFAULT '',
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `name` (`name`),
+  KEY `end` (`end`),
+  KEY `project` (`project`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `milestones_assigned`
+--
+
+DROP TABLE IF EXISTS `milestones_assigned`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `milestones_assigned` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `user` int(10) NOT NULL DEFAULT '0',
+  `milestone` int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `user` (`user`),
+  KEY `milestone` (`milestone`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `projectfolders`
+--
+
+DROP TABLE IF EXISTS `projectfolders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `projectfolders` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent` int(10) unsigned NOT NULL DEFAULT '0',
+  `project` int(11) NOT NULL DEFAULT '0',
+  `name` text NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `visible` text NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `project` (`project`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `projekte`
+--
+
+DROP TABLE IF EXISTS `projekte`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `projekte` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `desc` text NOT NULL,
+  `start` varchar(255) NOT NULL DEFAULT '',
+  `end` varchar(255) NOT NULL DEFAULT '',
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `budget` float NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `status` (`status`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `projekte_assigned`
+--
+
+DROP TABLE IF EXISTS `projekte_assigned`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `projekte_assigned` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `user` int(10) NOT NULL DEFAULT '0',
+  `projekt` int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `user` (`user`),
+  KEY `projekt` (`projekt`)
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `projects` text NOT NULL,
+  `tasks` text NOT NULL,
+  `milestones` text NOT NULL,
+  `messages` text NOT NULL,
+  `files` text NOT NULL,
+  `chat` text NOT NULL,
+  `timetracker` text NOT NULL,
+  `admin` text NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `roles_assigned`
+--
+
+DROP TABLE IF EXISTS `roles_assigned`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles_assigned` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `user` int(10) NOT NULL,
+  `role` int(10) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `settings`
+--
+
+DROP TABLE IF EXISTS `settings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `settings` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `settingsKey` varchar(50) NOT NULL,
+  `settingsValue` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tasklist`
+--
+
+DROP TABLE IF EXISTS `tasklist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tasklist` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `project` int(10) NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `desc` text NOT NULL,
+  `start` varchar(255) NOT NULL DEFAULT '',
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `access` tinyint(4) NOT NULL DEFAULT '0',
+  `milestone` int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `status` (`status`),
+  KEY `milestone` (`milestone`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tasks` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `start` varchar(255) NOT NULL DEFAULT '',
+  `end` varchar(255) NOT NULL DEFAULT '',
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `text` text NOT NULL,
+  `liste` int(10) NOT NULL DEFAULT '0',
+  `status` tinyint(1) NOT NULL DEFAULT '0',
+  `project` int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `liste` (`liste`),
+  KEY `status` (`status`),
+  KEY `end` (`end`)
+) ENGINE=MyISAM AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tasks_assigned`
+--
+
+DROP TABLE IF EXISTS `tasks_assigned`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tasks_assigned` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `user` int(10) NOT NULL DEFAULT '0',
+  `task` int(10) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `user` (`user`),
+  KEY `task` (`task`)
+) ENGINE=MyISAM AUTO_INCREMENT=108 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `timetracker`
+--
+
+DROP TABLE IF EXISTS `timetracker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `timetracker` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `user` int(10) NOT NULL DEFAULT '0',
+  `project` int(10) NOT NULL DEFAULT '0',
+  `task` int(10) NOT NULL DEFAULT '0',
+  `comment` text NOT NULL,
+  `started` varchar(255) NOT NULL DEFAULT '',
+  `ended` varchar(255) NOT NULL DEFAULT '',
+  `hours` float NOT NULL DEFAULT '0',
+  `pstatus` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ID`),
+  KEY `user` (`user`,`project`,`task`),
+  KEY `started` (`started`),
+  KEY `ended` (`ended`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT '',
+  `email` varchar(255) DEFAULT '',
+  `tel1` varchar(255) DEFAULT NULL,
+  `tel2` varchar(255) DEFAULT NULL,
+  `pass` varchar(255) DEFAULT '',
+  `company` varchar(255) DEFAULT '',
+  `lastlogin` varchar(255) DEFAULT '',
+  `zip` varchar(10) DEFAULT NULL,
+  `gender` char(1) DEFAULT '',
+  `url` varchar(255) DEFAULT '',
+  `adress` varchar(255) DEFAULT '',
+  `adress2` varchar(255) DEFAULT '',
+  `state` varchar(255) DEFAULT '',
+  `country` varchar(255) DEFAULT '',
+  `tags` varchar(255) DEFAULT '',
+  `locale` varchar(6) DEFAULT '',
+  `avatar` varchar(255) DEFAULT '',
+  `rate` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `name` (`name`),
+  KEY `pass` (`pass`),
+  KEY `locale` (`locale`)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-02-18 21:14:49
